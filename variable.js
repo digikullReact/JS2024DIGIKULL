d2957cb8d7211a156b92ef593e9f2062542d9659
shubh <shubhamdixit863@gmail.com>
2024-05-11 20:22:08 +0530

    js

---
+++ variable.js
@@ -0,0 +1,88 @@
+// Code for javascript
+// Variable declaration
+// variable are designed to store the data
+var  rams_apple=2;
+var shyam_Apples=2;
+// how many apple did ghnashyam distributed
+var ghanshyamApple=rams_apple+shyam_Apples;
+console.log("Ghanshyam Apple",ghanshyamApple);  // this prints the data in console
+//console.log("Ghanshyam Apple"+2);  // this prints the data in console
+
+// Data is evrything that you use in real world
+// Number
+//Text (alphabets) --->string
+// photos ---->binary
+//video --->binary data
+//files --->binary
+//boolean ---->true or false (yes or no)
+//
+
+var name="Aman";  // string data
+var age=24; // number data
+var isAgoostudent=true; // boolean data
+
+
+
+///  there are two other keywords that we use to create variables
+// let and const
+
+let c=9;
+const k="hello world";
+
+
+// variables operations
+let address="70/2 vijay nagar Gzb";
+
+
+
+
+
+// functions is like a container  in which you can put your code
+//  and you can execute them
+
+// function definition
+// with the help of function you can ship your code easily 
+function Add(){
+    // You can write your code
+    let c=9;
+    let f=90;
+    console.log(c+f);
+
+}
+
+
+// function calling
+Add()
+Add()
+Add()
+Add()
+Add()
+Add()
+Add()
+
+
+// Function helps us reduce the repeated logic
+
+
+
+
+//let multiply3=h3*j3;
+//console.log(multiply3s);
+
+
+function Multiply(h,j){  // parameter
+  
+    let multiply=h*j;
+    console.log(multiply);
+}
+Multiply(89,90); // argument
+Multiply(890,990);
+
+let y=9;
+y=90; // possible
+//let y=9; // not possible
+// Create functions for this 
+// Multiply  *
+// Addition  +
+// Subtraction -
+// Divison  /
