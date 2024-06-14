bd5d014bafc872067c8c34da3a6b66132b12144c
shubh <shubhamdixit863@gmail.com>
2024-06-14 21:37:40 +0530

    events

---
+++ dominput.js
@@ -0,0 +1,28 @@
+const btn=document.getElementById("btn");
+const input=document.getElementById("data");
+const heading=document.getElementById("heading");
+
+// btn.addEventListener("click",function(eventobject){
+//     //console.log(eventobject.target.innerHTML);
+//     const value=input.value;
+//     heading.innerText=value;
+
+// })
+
+// input.addEventListener("keypress",function(eventobject){
+//    // console.log();
+//    //const value=input.value;
+//     //heading.innerText=value;
+//     heading.innerText=eventobject.target.value
+// })
+
+
+// heading.addEventListener("mouseover",function(event){
+//     //heading.style.color="green";
+//     event.target.style.color="red";
+// })
+
+// heading.addEventListener("mouseleave",function(event){
+//     //heading.style.color="green";
+//     event.target.style.color="black";
+// })
