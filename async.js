40e73308de98f580b3cefef4aac2586c804e193d
shubh <shubhamdixit863@gmail.com>
2024-07-20 21:10:03 +0530

    js error callback

--- async.js
+++ async.js
@@ -92,3 +92,155 @@ function foo(){
 
 
   // synchronous callbacks vs asynchronous callbacks
+
+  //Error first callbacks
+// when ever you are doing any kind of activity 
+
+// will result in two things 
+// 1- error 
+// 2- result // success
+
+function sum_of_two_numbers(a, b, cb) {
+  setTimeout(() => {
+    if (typeof a == "number" && typeof b == "number") {
+      let sum = a + b;
+      cb(null, sum);
+    } else {
+      const err = new Error("passed values are not number")
+      cb(err, null);
+    }
+
+    ///return sum;
+    //console.log(sum);
+  }, 2000)
+
+}
+
+// sum_of_two_numbers(20, 10, function(error, result) {
+//   if (error != null) {
+//     console.log("There was error", error);
+//   } else {
+//     console.log(result);
+//   }
+
+// })
+
+// sum_of_two_numbers("a", 10, function(error, result) {
+//   if (error != null) {
+//     console.log("There was error", error);
+//   } else {
+//     console.log(result);
+//   }
+
+// })
+
+
+// Callback hell // pyramid of doom
+// its a pattern that gets formed automatically while dealing with dependent asynchronous call bak functions 
+
+// Given two numbers ,you have to check whether the sum of the numbers multiplied by product of the number ,divided by the subtraction of numbers is dibvisible by 2 or not
+// 4+2 -->6
+// 4*2--->8
+// 6*8 -->48
+// 4-2 -->2
+// 48/2 -->24
+// the only catch is the operations gonna happen asynchronously
+
+
+
+function add(a, b, cb) {
+  setTimeout(() => {
+    cb(a + b);
+  }, 2000)
+
+}
+
+
+function product(a, b, cb) {
+  setTimeout(() => {
+    cb(a * b);
+  }, 2000)
+
+}
+
+
+function subtract(a, b, cb) {
+  setTimeout(() => {
+    cb(a - b);
+  }, 2000)
+
+}
+
+
+function division(a, b, cb) {
+  setTimeout(() => {
+    cb(a / b);
+  }, 2000)
+
+}
+
+
+function divisibleby2(a, cb) {
+  setTimeout(() => {
+    if (a % 2 == 0) {
+      cb(true);
+    } else {
+      cb(false)
+    }
+
+  }, 2000)
+
+}
+
+//  callback hell 
+function opeation(a,b){
+  add(a,b,function(addresult){
+    // inside this
+    product(a,b,function(productresult){
+      // inside
+     // console.log(
+
+      subtract(a,b,(subtractresult)=>{
+        let r=addresult*productresult;
+        let dib=r/subtractresult
+       // console.log()
+        divisibleby2(dib,function(result){
+          if (result==true){
+            console.log("Number is divisble by 2");
+
+          }else{
+             console.log("Number is not  divisble by 2");
+          }
+
+        })
+
+      })
+
+    })
+
+  })
+
+
+
+}
+
+opeation(4,2);
+
+
+
+///   
+let country=[{name:"India",id:1},{name:"USA",id:2}]
+
+let state=[{name:"up",countryId:1,id:1},{name:"kansas",countryId:2,id:2}]
+
+// first function gives you the coubtry Id on the basis of name
+// second function gives you the stateId of on the basis of countryId
+// they have to be asynchronous 
+
+// Question is  -->given a countryName find out all the states in the country
+
+function find(countryName){
+  
+}
+
+find("India")
